de1a2fafdbd1d7c89082e0e6e63b1df7f3dd11a4
Lukasz Marek <marek.lucas@gmail.com>
2018-04-27 15:00:43 +0200

    code refactoring, added simple Character class

--- play.py
+++ play.py
@@ -10,6 +10,46 @@ class Direction(object):
     DOWN = 4
 
 
+class Character(object):
+    def __init__(self, image_up, image_down, image_left, image_right):
+        self._direction = Direction.LEFT
+        self._images = {Direction.UP: image_up, Direction.DOWN: image_down, Direction.LEFT: image_left,
+                        Direction.RIGHT: image_right}
+        self._rectangles = {Direction.UP: image_up.get_rect(), Direction.DOWN: image_down.get_rect(),
+                            Direction.LEFT: image_left.get_rect(), Direction.RIGHT: image_right.get_rect()}
+
+    @property
+    def image(self):
+        return self._images[self._direction]
+
+    @property
+    def rectangle(self):
+        return self._rectangles[self._direction]
+
+    @property
+    def direction(self):
+        return self._direction
+
+    def put(self, x, y):
+        for rectangle in self._rectangles.values():
+            rectangle.x = x
+            rectangle.y = y
+
+    def move(self, dx, dy):
+        for rectangle in self._rectangles.values():
+            rectangle.x += x_change if -rectangle.width / 2 < int(
+                rectangle.x + x_change) <= display_width - rectangle.width / 2 else 0
+            rectangle.y += y_change if -rectangle.height / 2 < int(
+                rectangle.y + y_change) <= display_height - rectangle.height / 2 else 0
+        self._change_direction(dx, dy)
+
+    def _change_direction(self, dx, dy):
+        if abs(dx) > abs(dy):
+            self._direction = Direction.UP if dx > 0 else Direction.DOWN
+        elif abs(dx) < abs(dy):
+            self._direction = Direction.RIGHT if dy > 0 else Direction.LEFT
+
+
 if __name__ == "__main__":
     import pygame
 
@@ -24,17 +64,15 @@ if __name__ == "__main__":
     GAME_DISPLAY.set_caption("Hack'n'Slash")
     clock = pygame.time.Clock()
 
-    CHARACTER_IDLE = pygame.transform.smoothscale(pygame.image.load("character_idle.png"),
+    idle_character = pygame.transform.smoothscale(pygame.image.load("character_idle.png"),
                                                   (int(display_width * 0.2), int(display_height * 0.2)))
 
-    CHARACTER_RECT = CHARACTER_IDLE.get_rect()
-
-    CHARACTER_RECT.x = display_width * 0.5
-    CHARACTER_RECT.y = display_height * 0.5
+    PLAYER = Character(idle_character, idle_character, idle_character, idle_character)
+    PLAYER.put(display_width * 0.5, display_height * 0.5)
 
 
-    def character_idle_position(rectangle):
-        SURFACE.blit(CHARACTER_IDLE, rectangle)
+    def character_idle_position(player):
+        SURFACE.blit(player.image, player.rectangle)
 
 
     player_quit = False
@@ -67,13 +105,10 @@ if __name__ == "__main__":
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
 
-        CHARACTER_RECT.x += x_change if -CHARACTER_RECT.width / 2 < int(
-            CHARACTER_RECT.x + x_change) <= display_width - CHARACTER_RECT.width / 2 else 0
-        CHARACTER_RECT.y += y_change if -CHARACTER_RECT.height / 2 < int(
-            CHARACTER_RECT.y + y_change) <= display_height - CHARACTER_RECT.height / 2 else 0
+        PLAYER.move(x_change, y_change)
 
         SURFACE.fill(Colour.WHITE)
-        character_idle_position(CHARACTER_RECT)
+        character_idle_position(PLAYER)
         pygame.display.update()
         clock.tick(60)
 
